8a346aaf0671a0f8c5c229e03698a2d3df9bcd56
MostafaMahmoudtv <mm4582@fayoum.edu.eg>
2025-10-14 04:48:39 +0300

    new

--- en/index.js
+++ en/index.js
@@ -398,18 +398,15 @@ if (slider) {
   let startX;
   let scrollLeft;
 
+  // Mouse Events (Desktop)
   slider.addEventListener("mousedown", (e) => {
     isDown = true;
     slider.classList.add("active");
     startX = e.pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
   });
-  slider.addEventListener("mouseleave", () => {
-    isDown = false;
-  });
-  slider.addEventListener("mouseup", () => {
-    isDown = false;
-  });
+  slider.addEventListener("mouseleave", () => (isDown = false));
+  slider.addEventListener("mouseup", () => (isDown = false));
   slider.addEventListener("mousemove", (e) => {
     if (!isDown) return;
     e.preventDefault();
@@ -417,7 +414,22 @@ if (slider) {
     const walk = (x - startX) * 1;
     slider.scrollLeft = scrollLeft - walk;
   });
+
+  // Touch Events (Mobile)
+  slider.addEventListener("touchstart", (e) => {
+    isDown = true;
+    startX = e.touches[0].pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("touchend", () => (isDown = false));
+  slider.addEventListener("touchmove", (e) => {
+    if (!isDown) return;
+    const x = e.touches[0].pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1;
+    slider.scrollLeft = scrollLeft - walk;
+  });
 }
+
 
 // ========== Scroll To Top ==========
 document.addEventListener("DOMContentLoaded", function () {
